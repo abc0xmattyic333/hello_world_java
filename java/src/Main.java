980700ce4c1d2b84ad8671286307e103ccde7bb6
abc0xmattyic333 <abc0xmattyic333@gmail.com>
2025-01-04 00:40:19 -0500

    add folders/files

---
+++ java/src/Main.java
@@ -0,0 +1,82 @@
+// single line comment in java
+
+/* 
+  this is a multi-line comment in java 
+*/
+
+// what is java?
+
+// java is a popular programming language, created in 1995.
+
+// it is owned by oracle, and more than 3 billion devices run java.
+
+// it is used for: 
+// 1. mobile applications (specially android apps)
+// 2. desktop applications
+// 3. web applications
+// 4. web servers and application servers
+// 5. games
+// 6. database connection
+// 7. and much, much more!
+
+// why use java?
+
+// java works on different platforms (windows, mac, linux, raspberry pi, etc.)
+// it also works on different devices (mobile, desktop, web, etc.)
+// java is an object-oriented programming language
+// java is a platform-independent language
+// java is easy to learn
+// java is open-source
+// java is secure, fast and powerful
+// java has a huge community support (tens of millions of developers)
+
+// java's slogan is: "write once, run anywhere"
+
+// this means that once you write a java program, you can run it on any device that supports java.
+
+// check to see if java is installed on your computer
+
+// open a command prompt (windows) or terminal (mac, linux)
+// type the following command: java -version
+// if you have java installed, you should see the output of the installed version.
+
+
+// create a hello world program in java
+
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello, java!");
+        System.out.println("my name is abc0xmattyic333");
+        System.out.println("i <3 computers and programming");
+    }
+}
+
+
+// run the program to see the output
+
+// the output should be: hello, java!
+
+// congratulations! you have written your first java program.
+
+// let's break down the program:
+
+// 1. package src;
+// this is a package declaration. a package is a namespace that organizes a set of related classes and interfaces.
+// a namespace is a collection of classes and interfaces that are grouped together.
+
+// 2. public class Main {}
+// this is a class declaration. a class is a blueprint for objects.
+// objects have states and behaviors. an object is created from a class.
+
+// 3. public static void main(String[] args) {}
+// this is a method declaration. a method is a block of code that performs a specific task.
+// in this case, the main method is the entry point of the program.
+
+// 4. System.out.println("hello, java!");
+// this is a statement. a statement is a complete command to be executed.
+// in this case, the statement prints the string "hello, java!" to the console.
+// a string is a sequence of characters.
+// a string can be enclosed in single quotes ('hello, java!') or double quotes ("hello, java!").
+// the println method is used to print a line of text to the console.
